dc239db02f5598d0046fed82dad77a28fc7ccd4c
wawanpi <muhaguskurniawan@gmail.com>
2023-05-15 21:30:18 +0700

    Constructor Dipanggil dan Destructor Dipanggil

--- Destruktor_202/Destruktor_202.cpp
+++ Destruktor_202/Destruktor_202.cpp
@@ -17,3 +17,31 @@ angka::angka(int i) {	//Constructor
 	arr = new int[i];
 	isiData();
 }
+
+angka::angka() { //Destructor
+	cout << endl;
+	cetakData();
+	delete[]arr;
+	cout << " Alamat Array Sudah Dilepaskan " << endl;
+}
+
+void angka::cetakData() {
+	for (int i = 1; i <= panjang; i++) {
+		cout << i << " = " << arr[i] << endl;
+	}
+}
+
+void angka::isiData() {
+	for (int i = 1; i <= panjang; i++) {
+		cout << i << " = "; cin >> arr[i];
+	}
+	cout << endl;
+}
+
+int main() {
+	angka belajarcpp(3);	//Constructor Dipanggil
+	angka* ptrBelajarcpp = new angka(5);	//Constructor
+	delete ptrBelajarcpp;	//Destructor Dipanggil
+
+	return 0;
+}	//Destructor Dipanggil
