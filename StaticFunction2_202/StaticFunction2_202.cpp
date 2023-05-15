ab067741888cc6615ee7facbddc47b08d5fa6b23
wawanpi <muhaguskurniawan@gmail.com>
2023-05-15 21:04:59 +0700

    membuat staticfunction

--- StaticFunction2_202/StaticFunction2_202.cpp
+++ StaticFunction2_202/StaticFunction2_202.cpp
@@ -12,5 +12,8 @@ public:
 	void setID();
 	void printAll();
 
+	static void setNim(int pNim) { nim = pNim; /*Definisi function*/ }
+	static int getNIm() { return nim; /*Definisi function*/ }
 
+	mahasiswa(string pnama) :nama(pnama) { setID(); }
 };
