f21cad24b8f47fb72fa2107eccabe78f0b65c535
wawanpi <muhaguskurniawan@gmail.com>
2023-05-15 21:37:34 +0700

    menampilkan nim dan nama

--- Konstructor_202/Konstructor_202.cpp
+++ Konstructor_202/Konstructor_202.cpp
@@ -6,3 +6,32 @@ class mahasiswa {
 private:
 	int nim;
 	string nama;
+public:
+	mahasiswa();
+	mahasiswa(int);
+	mahasiswa(string);
+	mahasiswa(int iNim, string iNama);
+	void cetak();
+};
+
+mahasiswa::mahasiswa() {
+	nim = 0;
+	nama = "";
+}
+
+mahasiswa::mahasiswa(int iNim) {
+	nim = iNim;
+}
+
+mahasiswa::mahasiswa(string iNama) {
+	nama = iNama;
+}
+
+mahasiswa::mahasiswa(int iNim, string iNama) {
+	nim = iNim;
+	nama = iNama;
+}
+void mahasiswa::cetak() {
+	cout << endl << " Nim =" << nim << endl;
+	cout << " Nama = " << nama << endl;
+}
