5ff70df2ede60e68e7f7b6e05ce655216e4f6dd7
wawanpi <muhaguskurniawan@gmail.com>
2023-05-15 20:56:50 +0700

    menampilkan id dan nama

--- StaticFunction_202/StaticFunction_202.cpp
+++ StaticFunction_202/StaticFunction_202.cpp
@@ -12,3 +12,15 @@ public:
 	void printALL();
 	mahasiswa(string pnama) :nama(pnama) { setID(); }
 };
+
+int mahasiswa::nim = 0;
+
+void mahasiswa::setID() {
+	id = ++nim;
+}
+
+void mahasiswa::printALL() {
+	cout << " ID =" << id << endl;
+	cout << " Nama =" << nama << endl;
+	cout << endl;
+}
